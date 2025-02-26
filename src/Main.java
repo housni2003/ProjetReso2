76c30a41fae61e5bed04d4c58d196915f69fcb60
nom1 <nom1@email.com>
2025-02-26 16:57:43 +0100

    testScanner

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         DatagramSocket test = new DatagramSocket(5);
 
         int startPort = 0;  // Exemple : plage de test
-        int endPort =20000;
+        int endPort =200;
 
         PortScannerUDP.scanUDPPorts(startPort, endPort);
     }
